43ad567839b66b0543350be6002a6b4f2e0f258d
ankit8volt <ankit@voltmoney.in>
2025-04-25 00:34:28 +0530

    handling db connections

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,19 +1,27 @@
 import { DataSource } from 'typeorm';
 import { Call } from '../entities/Call';
 import { config } from 'dotenv';
+import * as fs from 'fs';
+import * as path from 'path';
 
 config();
 
+const certPath = path.resolve(__dirname, '../../rds-ca-cert.pem');
+
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.POSTGRES_HOST || 'localhost',
+    host: process.env.POSTGRES_HOST || 'database-1.cy7pfbxr3mpl.ap-south-1.rds.amazonaws.com',
     port: parseInt(process.env.POSTGRES_PORT || '5432'),
-    username: process.env.POSTGRES_USER || 'bhavnaharitsa',
-    password: process.env.POSTGRES_PASSWORD || '',
-    database: process.env.POSTGRES_DB || 'sales_calls_db',
+    username: process.env.POSTGRES_USER || 'sairadb',
+    password: process.env.POSTGRES_PASSWORD || 'saira123',
+    database: process.env.POSTGRES_DB || 'postgres',
     synchronize: true, // Be careful with this in production
     logging: true,
     entities: [Call],
     subscribers: [],
     migrations: [],
+    ssl: {
+        ca: fs.readFileSync(certPath).toString(),
+        rejectUnauthorized: true
+    }
 }); 
